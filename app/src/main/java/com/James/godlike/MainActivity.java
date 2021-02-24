f8a7a2e19e3b1ec71878b5ab4e898494c90b82ee
xiaowq <18210088850@163.com>
2021-02-24 18:14:23 +0800

    god like

--- app/src/main/java/com/James/godlike/MainActivity.java
+++ app/src/main/java/com/James/godlike/MainActivity.java
@@ -1,14 +1,19 @@
 package com.James.godlike;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView main_title_txt = findViewById(R.id.main_title_txt);
+
+        main_title_txt.setText(BuildConfig.BaseUrl);
     }
 }
