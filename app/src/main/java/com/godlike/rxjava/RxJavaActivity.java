aed1b92cf4a3a643a1fcadaaa7b4084d5987a659
xiaowq <18210088850@163.com>
2021-05-27 16:04:57 +0800

    RXJava练习

--- app/src/main/java/com/godlike/rxjava/RxJavaActivity.java
+++ app/src/main/java/com/godlike/rxjava/RxJavaActivity.java
@@ -6,9 +6,6 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -26,6 +23,7 @@ public class RxJavaActivity extends AppCompatActivity {
     }
 
     private void initRxJava() {
+
         Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
@@ -35,51 +33,21 @@ public class RxJavaActivity extends AppCompatActivity {
             }
         });
 
-
-        // 1. 创建观察者 （Observer ）对象
         Observer<Integer> observer = new Observer<Integer>() {
-            // 2. 创建对象时通过对应复写对应事件方法 从而 响应对应事件
+            Disposable mDisposable;
 
-            // 观察者接收事件前，默认最先调用复写 onSubscribe（）
             @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "开始采用subscribe连接");
+            public void onSubscribe(@NonNull Disposable d) {
+                mDisposable = d;
             }
 
             // 当被观察者生产Next事件 & 观察者接收到时，会调用该复写方法 进行响应
             @Override
             public void onNext(Integer value) {
                 Log.d(TAG, "对Next事件作出响应" + value);
-            }
-
-            // 当被观察者生产Error事件& 观察者接收到时，会调用该复写方法 进行响应
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "对Error事件作出响应");
-            }
-
-            // 当被观察者生产Complete事件& 观察者接收到时，会调用该复写方法 进行响应
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "对Complete事件作出响应");
-            }
-        };
-
-
-        // 1. 创建观察者 （Observer ）对象
-        Subscriber<Integer> subscriber = new Subscriber<Integer>() {
-
-            // 2. 创建对象时通过对应复写对应事件方法 从而 响应对应事件
-            // 观察者接收事件前，默认最先调用复写 onSubscribe（）
-            @Override
-            public void onSubscribe(Subscription s) {
-                Log.d(TAG, "开始采用subscribe连接");
-            }
-
-            // 当被观察者生产Next事件 & 观察者接收到时，会调用该复写方法 进行响应
-            @Override
-            public void onNext(Integer value) {
-                Log.d(TAG, "对Next事件作出响应" + value);
+                if (value == 2) {
+                    mDisposable.dispose();
+                }
             }
 
             // 当被观察者生产Error事件& 观察者接收到时，会调用该复写方法 进行响应
